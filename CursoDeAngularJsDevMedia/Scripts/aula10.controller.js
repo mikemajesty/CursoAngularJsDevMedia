f3cb2da6d9888e752cf3759467d243fb0ac970cc
mikemajesty <mikemajesty2011@gmail.com>
2015-11-25 21:17:52 -0200

    Aula 10 - Filter

---
+++ CursoDeAngularJsDevMedia/Scripts/aula10.controller.js
@@ -0,0 +1,12 @@
+﻿function Aula10controller($scope,$filter) {
+    console.log("Iniciou o serviço");
+    //tem que adicionar o Filter, na assinatura do metodo, padrão angular kkkk
+    //console.log(retornaoiFilter("Digimon"));
+    console.log($filter("retornaoi")("Agumon"));
+    $scope.pessoa = {
+        nome: "Mike Lima",
+        idade: 26,
+        estado: "SP"
+    };
+}
+app.controller("Aula10controller", Aula10controller);
---
+++ CursoDeAngularJsDevMedia/Scripts/aula10.filter.js
@@ -0,0 +1,5 @@
+﻿app.filter("retornaoi", function () {
+    return function (nome) {
+        return "Olá :" + nome+"!";
+    }
+});
