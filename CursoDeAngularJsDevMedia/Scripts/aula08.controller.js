90721b4aebeb247b597f55b6eae93db4d9bc8af4
mikemajesty <mikemajesty2011@gmail.com>
2015-11-25 11:22:04 -0200

    Aula 08 -  Trabalhando com formulários

---
+++ CursoDeAngularJsDevMedia/Scripts/aula08.controller.js
@@ -0,0 +1,17 @@
+﻿app.controller("aula08.controller", function ($scope) {
+    $scope.pessoa = novaPessoa();
+    $scope.estados = ["RS", "SP", "RJ", "PR", "SC"];
+
+    $scope.pessoas = [];
+    $scope.salvarPessoa = function (pessoa) {
+        $scope.pessoas.push(pessoa);
+        console.log($scope.pessoas);
+        $scope.pessoa = novaPessoa();
+        $scope.form.$setUntouched();       
+        $scope.form.$setPristine();
+    }
+});
+
+function novaPessoa() {
+    return { nome: "", email: "", sexo: "F",estado: "SP" }
+}
