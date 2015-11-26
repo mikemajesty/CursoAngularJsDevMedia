74b0a2b2d081f39ee57f6c2d3ec9ea4f1e67776b
mikemajesty <mikemajesty2011@gmail.com>
2015-11-26 10:50:12 -0200

    Aula 12 - Diretivas básicas

---
+++ CursoDeAngularJsDevMedia/Scripts/aula12.controller.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module("app", ["ngSanitize"]);
+function Aula12controller($scope) {
+    $scope.mostrar = false;
+    $scope.variavelHtml = "<h1>seja bem vindo mike rodrigues de lima</h1>";
+    $scope.minhaClass = "";
+    $scope.variavelCloack = "meu texto muito grandão";
+    $scope.minhaFoto = "foto1.png";
+}
+app.controller("aula12controller", Aula12controller);
