d70d93d1821276dd9b534244cb9eb8638cd5f981
mikemajesty <mikemajesty2011@gmail.com>
2015-11-26 09:39:15 -0200

    Aula 11  - ngRoute

---
+++ CursoDeAngularJsDevMedia/Scripts/aula11.controller.js
@@ -0,0 +1,37 @@
+﻿angular.module("app", ["ngRoute"])
+.controller("Aula11controller", function ($scope, $route, $routeParams, $location) {
+    $scope.$route = $route;
+    $scope.$location = $location;
+    $scope.$routeParams = $routeParams;
+})
+    .controller("InicialController", function ($scope) {
+        console.log("Entrou no controle incial");
+    })
+    .controller("Pagina1Controller", function ($scope) {
+        console.log("Entrou no controle 1");
+    })
+    .controller("Pagina2Controller", function ($scope, $routeParams) {
+        $scope.parametros = $routeParams;
+        console.log("Entrou no controle 2 com o produto com o id: " + $scope.parametros.idproduto);
+    })
+.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+    .when('/',
+    {
+        templateUrl: 'aula11.inicial.html',
+        controller: 'InicialController'
+    })
+        .when('/pagina1', {
+            templateUrl: 'aula11.pagina1.html',
+            controller: 'Pagina1Controller'
+        })
+        .when('/pagina2/:idproduto', {
+            templateUrl: 'aula11.pagina2.html',
+            controller: 'Pagina2Controller'
+        })
+    .otherwise
+    ({
+        redirectTo: '/'
+    });
+    $locationProvider.html5Mode(true);
+});
