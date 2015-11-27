b94b982eacb3c1436bb3c0e5046863082386fa84
mikemajesty <mikemajesty2011@gmail.com>
2015-11-27 08:47:31 -0200

    Aula 13 - Criando diretivas customizadas

---
+++ CursoDeAngularJsDevMedia/Scripts/aula13.controller.js
@@ -0,0 +1,33 @@
+﻿
+function Aula13controller($scope) {
+    $scope.variavel = "Mike Rodrigues de lima"
+    console.log("Entrou no controller");
+}
+app.controller("aula13controller", Aula13controller);
+
+app.directive("ngOla", function () {
+    console.log("Entrou na directive");
+    return {
+        //onde o [A] representa que sera exibido em um formato HTML
+        //onde o [E] representa que sera exibido em um Elemento HTML personalizado
+        //onde o [C]representa que sera exibido em um Elemento class
+        restrinct: 'A',
+        scope:{
+            ngNome :'@'
+        },
+        template: '<h1>Ola Mike - {{ngNome}}</h1>',
+        controller: ['$scope', function ($scope) {
+            $scope.digaOla = function (nome) {
+                alert('Ola '+ nome+"!");
+            };
+        }],
+        link: function (scope, iElement, iAttrs) {
+            console.log("Entrou no link");
+            console.log("Entrou no link: scope" + scope);
+            console.log("Entrou no link: iElement" + iElement);
+            console.log("Entrou no link: iAttrs" + iAttrs);
+            scope.digaOla(iAttrs.ngNome);
+        }
+}
+});
+
