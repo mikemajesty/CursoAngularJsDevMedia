05ff9b0d3ff2aa0733b7d7ace138e5a2e8c5ca56
mikemajesty <mikemajesty2011@gmail.com>
2015-11-28 13:29:36 -0200

    Aula 15  - Acesso a dados usando $resource

---
+++ CursoDeAngularJsDevMedia/Scripts/aula15.controller.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module('app', ['ngResource']);
+function Aula15controller($scope, Livros) {
+    console.log("Entrou no Controller")
+    $scope.carregaLivros = function () {
+        Livros.get();
+    }
+    $scope.getLivro = function (livroid) {
+        Livros.get({ livroid: livroid }, function () {
+            console.log("Entrou no  Livros.get")
+        });
+    }
+    $scope.salvarLivro = function () {
+        livro = {
+            "id": 40,
+            "titulo": "DevMedia"
+        };  
+        Livros.save({},livro);
+    }
+}
+app.factory('Livros', ['$resource', function ($resource) {
+    console.log("Entrou na Função Listas")
+    return $resource('/Scripts/Aula15.dados.json')
+}]);
+app.controller("aula15controller", Aula15controller);
