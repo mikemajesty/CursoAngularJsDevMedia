3b2c4d72a658dc2e5caf0d7f5706aec9dabb3377
mikemajesty <mikemajesty2011@gmail.com>
2015-11-28 14:52:12 -0200

    Aula 16  - AngularJS vs jQuery

---
+++ CursoDeAngularJsDevMedia/Scripts/aula16.controller.js
@@ -0,0 +1,21 @@
+﻿
+function Aula16controller($scope) {
+    $scope.mike = "Mike LIma";
+}
+app.directive('tooltip', function () {
+    debugger;
+    console.log("Entrou na diretiva");
+    return {
+        restrict: 'A',
+        link: function (scope, element, attr) {
+            console.log(scope);
+            console.log(element);
+            console.log(attr);
+            alement.show({
+                offset: [0, 10]
+            });
+        }
+    };
+});
+app.controller("Aula16controller", Aula16controller);
+
