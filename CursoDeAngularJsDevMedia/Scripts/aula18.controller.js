d989007ca40a27a519c75b9194d3057a46de79c2
mikemajesty <mikemajesty2011@gmail.com>
2015-11-29 16:29:37 -0200

    Aula 18 - $location

---
+++ CursoDeAngularJsDevMedia/Scripts/aula18.controller.js
@@ -0,0 +1,18 @@
+﻿
+function Aula18controller($scope,$location) {
+    $scope.$location = $location;
+    $scope.setID = function (_id) {
+        $location.search({id:_id});
+    }
+    $scope.$watch('$location.search().id', function (id) {
+        console.log("Mudou o valor do ID: "+id);
+    })
+}
+
+app.controller("Aula18controller", Aula18controller).config(function ($locationProvider) {
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    }).hashPrefix('#');
+});
+
