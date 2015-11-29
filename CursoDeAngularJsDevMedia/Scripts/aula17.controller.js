29238d50816020ccf85a1b44c3a2907f18ae01b8
mikemajesty <mikemajesty2011@gmail.com>
2015-11-29 16:01:15 -0200

    Aula 17 - Animations

---
+++ CursoDeAngularJsDevMedia/Scripts/aula17.controller.js
@@ -0,0 +1,13 @@
+﻿var app = angular.module('app', ['ngAnimate']);
+function Aula17controller($scope) {
+    $scope.itens = ['1','2','3','4','5'];
+    $scope.adicionaItem = function () {
+        if ($scope.itens.indexOf($scope.itemadd) == -1) {
+            $scope.itens.push($scope.itemadd);
+        }
+       
+    }
+}
+
+app.controller("Aula17controller", Aula17controller);
+
