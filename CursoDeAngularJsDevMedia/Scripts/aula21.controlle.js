353c9b0e61056a88ce8e2e82abb80d2b85c707d8
mikemajesty <mikemajesty2011@gmail.com>
2015-11-30 16:44:30 -0200

    Aula 21 - Upload de Arquivos

---
+++ CursoDeAngularJsDevMedia/Scripts/aula21.controlle.js
@@ -0,0 +1,26 @@
+﻿
+var app = angular.module('app', ['angularFileUpload']);
+
+
+function Aula21controller($scope,FileUploader) {
+    var uploader = $scope.uploader = new FileUploader({
+        url: "service/upload.php"
+    });
+    uploader.filters.push({
+        name: "tamanhoFila",
+        fn: function (item,options) {
+            return this.queue.lenght < 4;
+        }
+    });
+    uploader.onSuccessItem = function (fileItem) {
+        console.log("Item enviado com sucesso");
+        fileItem.remove();
+    }
+    //falha ao adicionar o arquivo
+    uploader.onWhenAddingFileFailed = function (fileItem) {
+        console.log("Erro ao adicionar o elemento",fileItem);
+    }
+
+}
+app.controller("Aula21controller", Aula21controller);
+
