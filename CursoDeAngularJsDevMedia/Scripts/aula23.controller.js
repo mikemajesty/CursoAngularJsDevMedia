e0ae758135e1a7dab881badb0cc11778a5f6fe72
mikemajesty <mikemajesty2011@gmail.com>
2015-11-30 18:48:19 -0200

    Aula 23 - AngularUI: ui.unique e ui.validate

---
+++ CursoDeAngularJsDevMedia/Scripts/aula23.controller.js
@@ -0,0 +1,12 @@
+﻿
+var app = angular.module('app', ['ui.unique','ui.validate']);
+
+
+function Aula23Controller($scope) {
+    $scope.usuario = [{ nome: 'mike', idade: 26 }, { nome: 'mike 1' , idade: 26 }, { nome: 'mike 1', idade: 27 }, { nome: 'mike', idade: 27 }];
+    $scope.validar = function (valor) {
+        return valor === $scope.senha;
+    }
+}
+app.controller("Aula23Controller", Aula23Controller);
+
