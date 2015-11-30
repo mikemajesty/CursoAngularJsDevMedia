db4e9699e93d02b0d3eb0dec2877e111b1f70702
mikemajesty <mikemajesty2011@gmail.com>
2015-11-30 18:06:16 -0200

    Aula 22 - AngularUI: ui.highlight e ui.mask

---
+++ CursoDeAngularJsDevMedia/Scripts/aula22.controller.js
@@ -0,0 +1,17 @@
+﻿
+var app = angular.module('app', ['ui.highlight', 'ui.mask', 'ngSanitize']);
+
+
+function Aula22controller($scope) {
+
+    $scope.texto = "essa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introduçãoessa é uma breve introdução";
+    $scope.maskCPF = '999.999.999-99';
+    $scope.maskPhone = '(99) 99999-9999';
+    /*
+    9 somente numeros;
+    A somente letras
+    * qualquer coisa
+    */
+}
+app.controller("Aula22controller", Aula22controller);
+
