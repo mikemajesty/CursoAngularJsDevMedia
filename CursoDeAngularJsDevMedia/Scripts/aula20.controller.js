f1500c5fd9de1f19698df8e1f2184ffca31f2ce5
mikemajesty <mikemajesty2011@gmail.com>
2015-11-30 16:01:32 -0200

    Aula 20 -  Plugins

---
+++ CursoDeAngularJsDevMedia/Scripts/aula20.controller.js
@@ -0,0 +1,29 @@
+﻿
+var app = angular.module('app', ['pascalprecht.translate']);
+app.config(function ($translateProvider) {
+    $translateProvider.translations('pt-br', {
+        TITULO: "seja bem vindo",
+        PARAGRAFO: "Essa é uma frase de inicio",
+        BTN_OK: "Confirmar",
+        BTN_CANCEL: "Cancelar",
+        EN_EN: "English",
+        PT_BR: "Portugues"
+    });
+    $translateProvider.translations('en_en', {
+        TITULO: "This is a Hello Page",
+        EN_EN: "English",
+        PT_BR: "Portugues",
+        PARAGRAFO: "this is a start phrase",
+        BTN_OK: "Confirm",
+        BTN_CANCEL: "Cancel"
+    });
+    $translateProvider.preferredLanguage('pt-br');
+});
+
+function Aula20controller($scope, $translate) {
+    $scope.setaIdioma = function (sigla) {
+        $translate.use(sigla);
+    }
+}
+app.controller("Aula20controller", Aula20controller);
+
