ab4ac42de39c6e0a31ba917d1f890d4a02c20d5f
mikemajesty <mikemajesty2011@gmail.com>
2015-12-01 11:41:16 -0200

    Aula 26  - AngularUI: ui.bootstrap

---
+++ CursoDeAngularJsDevMedia/Scripts/aula26.controller.js
@@ -0,0 +1,15 @@
+﻿var app = angular.module('app', ['ui.bootstrap']);
+
+function Aula26Controller($scope) {
+
+    console.log('entrou');
+    $scope.accor = { heading: "", conteudo: "" };
+
+    $scope.addAccordion = function () {
+        $scope.grupos.push($scope.accor);
+        $scope.accor = { heading: "", conteudo: "" };
+    }
+    $scope.grupos = [{ heading: "dynamic contet", conteudo: "Group's dynamic contet" }, { heading: "dynamic contet", conteudo: "Group's dynamic contet" }, { heading: "dynamic contet", conteudo: "Group's dynamic contet" }];
+}
+app.controller("Aula26Controller", Aula26Controller);
+
