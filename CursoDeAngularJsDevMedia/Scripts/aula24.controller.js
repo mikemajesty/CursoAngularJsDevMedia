74cde20e6087f61f728ccc818552dec90e605f17
mikemajesty <mikemajesty2011@gmail.com>
2015-12-01 10:06:49 -0200

    Aula 24 - Google Maps

---
+++ CursoDeAngularJsDevMedia/Scripts/aula24.controller.js
@@ -0,0 +1,16 @@
+﻿function onGloogleReady() {
+    console.log("entrou na função");
+    return angular.bootstrap(document.getElementsByTagName('body')[0], ['app']);
+}
+angular.module('app', ['uiGmapgoogle-maps'])
+       .controller("Aula24Controller", function ($scope) {
+           debugger;
+           console.log("entrou no controller");
+           $scope.map = { center: { latitute: 45, longitude: -73 }, zoom: 10 };
+           $scope.marker = {
+               id: 0,
+               coords: {
+                   latitute: 45, longitude: -73
+               }
+           };
+       });
